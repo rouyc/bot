fa3635472c2737ed79495c4851baa54abbc37f67
rouyc <rouyclement@gmail.com>
2021-06-22 22:07:51 +0200

    MEP Bet Euro

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {prefix, channel_id,channel_role_id, channel_euro_id, role_euro_id, emoji_
 const token = startToken + midToken + endToken;
 const clientDiscord = new Discord.Client();
 const restDBConfig = {
-  'baseURL': 'https://euro2020-46ed.restdb.io/rest/',
+  'baseURL': 'https://euro2020-46ed.restdb.io/rest',
   'headers': {
     'Content-Type': 'application/json',
     'cache-Control': 'no-cache',
@@ -43,24 +43,32 @@ clientDiscord.on('message', message => {
         message.delete();
       break;
       case ('/help') :
-        clientDiscord.channels.cache.get(channel_id).send("```Commandes du bot :" +
+        clientDiscord.channels.cache.get(channel_id).send(">>> Commandes du bot :" +
             "\n - " + prefix + "help : Affiche toutes les commandes disponibles" +
-            /*"\n - " + prefix + "serveurNom : Donne le nom du serveur" +
-            "\n - " + prefix + "serveurMembre : Nombre de gens sur le serveur" +
-            "\n - " + prefix + "serveurMembreConnecte : Nombre de gens connectés sur le serveur" +
-            "\n - " + prefix + "serveurMembreDeconnecte : Nombre de gens deconnectés sur le serveur" +
-            "\n - " + prefix + "photo : Envoie une photo" +*/
-            "\n - " + prefix + "vote + intitulité : Propose un vote" +
-            "```");
+            "\n - " + prefix + "vote + intitulité : Propose un vote");
         message.delete();
-      break;
+        break;
+    }
+  }
+  if (message.channel.id === channel_role_id) {
+    let input = message.content.split(" ");
+    let commande = input[0];
+    switch(commande){
+      case ("/set") :
+        clientDiscord.channels.cache.get(channel_role_id).send(">>> Pour obtenir le rôle souhaité réagissez avec l'émoji associé :" +
+            "\n - <:euro2020:856854074144981002> Pour obtenir le rôle Euro et faire des pronos sur l'Euro")
+            .then(function (message) {
+              message.react('856854074144981002');
+            });
+        message.delete();
+        break;
     }
   }
   if (message.channel.id === channel_euro_id) {
     let input = message.content.split(" ");
     let commande = input[0];
     switch(commande){
-      case ("/euro") :
+      case ("/euro-bet") :
         let match = input.slice(1);
         let date = new Date().toJSON().slice(0,19).replace(/-/g,'/');
         let dateFormat = date.split('T')[0] + " " + date.split('T')[1];
@@ -79,12 +87,33 @@ clientDiscord.on('message', message => {
             .then(response => console.log(response))
             .catch(error => console.log(error))
         message.delete();
-      break;
+        break;
+      case ("/euro-view-my-bet") :
+        let id_member = '"' + message.member.id + '"';
+        axiosRestDBConfig.get('/prono?q={"id_member":' + id_member + '}')
+            .then(response =>
+                response.data.forEach(function(match) {
+                  console.log(match);
+                  let embed = new Discord.MessageEmbed()
+                      .setColor("#0099ff")
+                      .setTitle(match.match)
+                      .addFields(
+                          { name: 'Vainqueur', value: match.resultat, inline: true },
+                          { name: 'Score', value: match.score, inline: true },
+                          { name: ' Buteur', value: match.buteur, inline: true }
+                      )
+                      .setTimestamp(match.date)
+                  message.author.send(embed)
+                })
+            )
+            .catch(error => console.log(error))
+        message.delete();
+        break;
       case ('/help') :
-        clientDiscord.channels.cache.get(channel_euro_id).send("```Commandes du bot :" +
+        clientDiscord.channels.cache.get(channel_euro_id).send(">>> Commandes du bot :" +
             "\n - " + prefix + "help : Affiche toutes les commandes disponibles" +
-            "\n - " + prefix + "euro : Pour établir un prono sur l'Euro respecter la mise en forme : /euro France-Belgique France 1-0(Facultatif) Umtiti(Facultatif)" +
-            "```");
+            "\n - " + prefix + "euro-bet : Pour établir un prono sur l'Euro respecter la mise en forme : /euro-bet France-Belgique France 1-0(Facultatif) Umtiti(Facultatif)" +
+            "\n - " + prefix + "euro-view-my-bet : Pour voir ses pronos faits");
         message.delete();
         break;
     }
@@ -112,55 +141,5 @@ clientDiscord.on('messageReactionAdd', async (reaction, user) => {
     }
   }
 })
-
-/*clientDiscord.on('message', message => {
-  if (message.channel.id === channel_id) {
-    if (message.content === prefix + "serveurNom") { //Nom du serveur
-      message.reply("Serveur " + message.guild.name);
-    }
-    if (message.content === prefix + "serveurMembre") { //Nombre de membre sur le serveur
-      clientDiscord.channels.get("694260267885789215").send("Il y a actuellement " + message.guild.memberCount + " membres");
-      clientDiscord.channels.get("694253678617427989").send("Commande serveurMembre");
-      message.delete();
-    }
-    if (message.content === prefix + "serveurMembreConnecte") { //Nombre de membre connecte sur le serveur
-      clientDiscord.channels.get("694260267885789215").send("Il y a actuellement " + message.guild.members.filter(({presence}) => presence.status !== 'offline').size + " membre(s) connecté(s)");
-      clientDiscord.channels.get("694253678617427989").send("Commande serveurMembreConnectes");
-      message.delete();
-    }
-    if (message.content === prefix + "serveurMembreDeconnecte") { //Nombre de membre deconnecte sur le serveur
-      clientDiscord.channels.get("694260267885789215").send("Il y a actuellement " + message.guild.members.filter(({presence}) => presence.status == 'offline').size + " membre(s) déconnecté(s)");
-      clientDiscord.channels.get("694253678617427989").send("Commande serveurMembreDeconnectes");
-      message.delete();
-    }
-    if (message.content === prefix + "photo") { //Envoyer une photo
-      clientDiscord.channels.get(channel_id).send("Bip Bip Boup", {files: ["./images/robot.png"]});
-    }
-    if (message.content.startsWith(prefix + "vote")) { //Initier un vote
-      let args = message.content.split(" ").slice(1);
-      let intitule = args.join(" ");
-      let embed = new Discord.MessageEmbed()
-          .addField(intitule, "Répondre avec :white_check_mark: ou :x:")
-          .setColor("0xB40404")
-          .setTimestamp();
-      message.reply(embed) //Crée le vote dans le channel vote
-          .then(function (message) {
-            message.react('✅');
-            message.react('❌');
-          });
-    }
-    if (message.content === prefix + "help") {
-      clientDiscord.channels.cache.get(channel_id).send("```Commandes du bot :" +
-          "\n - " + prefix + "help : Affiche toutes les commandes disponibles" +
-          /!*"\n - " + prefix + "serveurNom : Donne le nom du serveur" +
-           "\n - " + prefix + "serveurMembre : Nombre de gens sur le serveur" +
-           "\n - " + prefix + "serveurMembreConnecte : Nombre de gens connectés sur le serveur" +
-           "\n - " + prefix + "serveurMembreDeconnecte : Nombre de gens deconnectés sur le serveur" +
-           "\n - " + prefix + "photo : Envoie une photo" +*!/
-          "\n - " + prefix + "vote + intitulité : Propose un vote" +
-          "```");
-      }
-  }
-});*/
 
 clientDiscord.login(token);
