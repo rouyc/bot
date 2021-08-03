ab2c7c32a1273f9db66cc85c8dcee6da13fc7531
rouyc <rouyclement@gmail.com>
2021-08-04 00:17:44 +0200

    Ajout Tickets

--- config/emoji_id.js
+++ config/emoji_id.js
@@ -2,7 +2,7 @@ let emoji_bot_id = "857310806513418250";
 let emoji_euro_id = "856854074144981002";
 let emoji_musique_id = "870977864667922453";
 let emoji_valorant_id = "871171679680884788";
-let emoji_tickets_id = "8872219730872782948";
+let emoji_tickets_id = "872219730872782948";
 
 module.exports = {
     emoji_bot_id: emoji_bot_id,
